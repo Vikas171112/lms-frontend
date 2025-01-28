a0aa302c55e8eef8b71a5d6b045699d1bd69c61c
Vikas Jha <vjha6510@gmail.com>
2025-01-28 13:35:02 +0530

    added basic Course Slice

---
+++ src/Redux/Slices/courseSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  courseList: [],
+};
+const courseSlice = createSlice({
+  name: "course",
+  initialState,
+  reducers: {},
+});
+export default courseSlice.reducer;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../Redux/Slices/authSlice.js";
+import courseReducer from "../Redux/Slices/courseSlice.js";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    course: courseReducer,
   },
   devtools: true,
 });
